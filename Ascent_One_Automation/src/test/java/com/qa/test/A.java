a2aee902c4e4e9eb5acc6846c159fccb632fa874
Vikas.Verma <Vikas.Verma@TNINCHD115.oca.internal>
2025-04-15 12:08:18 +0530

    Setup commit

---
+++ Ascent_One_Automation/src/test/java/com/qa/test/A.java
@@ -0,0 +1,34 @@
+package com.qa.test;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.ascentone.qa.base.TestBase;
+import com.cfm.qa.pages.login.LoginPage;
+
+
+public class A extends TestBase{
+	
+	LoginPage lPage;
+	
+//	@BeforeClass()
+	public void preRequisite() throws Exception {
+		openBrowser();
+		enterBaseURL();
+		lPage = new LoginPage(driver);
+		lPage.loginToApp();
+
+	}
+	
+	@Test()
+	public void launchBrowser() throws Exception {
+		
+		openBrowser();
+		enterBaseURL();
+		lPage = new LoginPage(driver);
+		lPage.loginToApp();
+		
+	}
+
+}
